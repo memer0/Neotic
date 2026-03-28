2fb061aeb10aa0fe3c0480b9728804b536464952
toxicbishop <92860129+toxicbishop@users.noreply.github.com>
2026-03-28 09:44:48 +0530

    Deployment & Environment
    Deployment & Environment

--- server/gen-certs.ps1
+++ server/gen-certs.ps1
@@ -2,15 +2,24 @@
 # PSAvoidUsingPlainTextForPassword: skip
 $cert = New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation "Cert:\CurrentUser\My" -FriendlyName "Neotic Local SSL"
 
+# Create a dedicated certificates folder at root if it doesn't exist
+$cert_dir = Join-Path (Get-Location) "certificates"
+if (-not (Test-Path $cert_dir)) {
+    New-Item -Path $cert_dir -ItemType Directory
+}
+
 # Constructing the secure string directly via object to avoid the ConvertTo-SecureString -AsPlainText linter flag
 $rawPass = "neotic"
 $pfx_password = New-Object System.Security.SecureString
 $rawPass.ToCharArray() | ForEach-Object { $pfx_password.AppendChar($_) }
 $pfx_password.MakeReadOnly()
 
-Export-PfxCertificate -Cert $cert -FilePath "./server/neotic.pfx" -Password $pfx_password
-Export-Certificate -Cert $cert -FilePath "./server/neotic.crt"
+# Exporting certificates to the ignored directory
+Export-PfxCertificate -Cert $cert -FilePath (Join-Path $cert_dir "neotic.pfx") -Password $pfx_password
+Export-Certificate -Cert $cert -FilePath (Join-Path $cert_dir "neotic.crt")
 
+# Local Development Note:
+# For uvicorn to use these easily, we'll use uvicorn's builtin ssl options.
 # Uvicorn works better with .pem files.
 # For simplicity, we'll use Next.js's built-in mkcert automation which is more standard.
 # Note: For uvicorn to use these easily, we'll use uvicorn's builtin ssl options.
